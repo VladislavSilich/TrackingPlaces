cccf3b64f6f1522a1c2b579ce78325fd9dd5c793
VLADISLAV\Lenovo <vladislav.silich.1996@gmail.com>
2017-07-18 00:45:07 +0300

    added display of location on the map

---
+++ Task/app/src/main/java/com/task/example/silich/vladislav/task/LocationManger.java
@@ -0,0 +1,107 @@
+package com.task.example.silich.vladislav.task;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * Created by Lenovo on 18.07.2017.
+ */
+
+public class LocationManger implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    private final String TAG = "MyAwesomeApp";
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private static Context context;
+    private LocationManger() {
+    }
+    private static LocationManger manager;
+    public static LocationManger getInstance(Context _context) {
+        if (manager == null) {
+            manager = new LocationManger();
+        }
+        context = _context;
+        return manager;
+    }
+    public void setUpLocation() {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (servicesConnected()) {
+            startPeriodicUpdates();
+        }
+    }
+    private void startPeriodicUpdates() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000); // Update location every second
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        if (mGoogleApiClient.isConnected())
+            System.out.println("destroy2");
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+    public void stopLocationUpdate() {
+        stopPeriodicUpdates();
+    }
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            // Continue
+            return true;
+            // Google Play services was not available for some reason
+        } else {
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, (Activity) context, 0);
+            if (dialog != null) {
+            }
+            return false;
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has been suspend");
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection has failed");
+        ((LocationFound) context).locationFailed();
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        ((LocationFound) context).locationFound(location);
+        stopLocationUpdate();                //stop location update. comment this line if you wish to get updated location latitude and longitude
+    }
+    public interface LocationFound {
+        public void locationFound(Location location);
+        public void locationFailed();
+    }
+}
--- Task/app/src/main/java/com/task/example/silich/vladislav/task/MapActivity.java
+++ Task/app/src/main/java/com/task/example/silich/vladislav/task/MapActivity.java
@@ -1,5 +1,7 @@
 package com.task.example.silich.vladislav.task;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,27 +10,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements LocationManger.LocationFound {
     SupportMapFragment mapFragment;
     GoogleMap googleMap;
+    LocationManager locationManager;
+    String PROVIDER = LocationManager.GPS_PROVIDER;
+    private Location mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        LocationManger locationManger=LocationManger.getInstance(this);
+        locationManger.setUpLocation();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+
         googleMap = mapFragment.getMap();
         if (googleMap == null){
             finish();
             return;
         }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -60,5 +75,29 @@ public class MapActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void locationFound(Location location) {
+        Float speed=location.getSpeed();
+        double latitude=location.getLatitude();
+        double longitude=location.getLongitude();
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(13)
+                .bearing(45)
+                .tilt(20)
+                .build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        googleMap.animateCamera(cameraUpdate);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)));
+        Toast.makeText(MapActivity.this, "latitude" + latitude + "longitude" + longitude, Toast.LENGTH_LONG).show(); //show your latitude and longitude on toast
+    }
+
+    @Override
+    public void locationFailed() {
+
     }
 }
